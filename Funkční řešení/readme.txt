0f5a7bb2bfc78e7c68b14425a28dc06efd685261
Martin Rampouch <m.rampouch@outlook.com>
2025-05-19 00:32:42 +0200

    cahnges

--- Funkční řešení/readme.txt
+++ Funkční řešení/readme.txt
@@ -4,29 +4,15 @@
 https://www.kaggle.com/datasets/cisautomotiveapi/large-car-dataset
 
 
-##
+## CAR
 https://deepvisualmarketing.github.io/
 
-
-https://www.kaggle.com/datasets/austinreese/craigslist-carstrucks-data
-
-
-
-Food delivery 
-
-https://www.actowizsolutions.com/foodora-food-data-scraping.php
-
-Uber eats
-https://www.actowizsolutions.com/uber-eats-food-menu-prices-reviews-delivery-dataset.php
-
+ 
 Wolt
 https://www.kaggle.com/datasets/muhammadwajeeharif/wolt-delivery-dataset
 https://github.com/dmuiruri/woltorders
 
 
-
-
-https://www.kaggle.com/datasets/heeraldedhia/groceries-dataset
 
 
 
--- Funkční řešení/scripts/manual-cluster-init.sh
+++ Funkční řešení/scripts/manual-cluster-init.sh
@@ -13,14 +13,12 @@ docker compose exec shard01-a bash scripts/init-shard01.js
 docker compose exec shard02-a bash scripts/init-shard02.js
 docker compose exec shard03-a bash scripts/init-shard03.js
 
-
 # === WAITING FOR MONGOS ===
 echo "Čekám, než bude router01 připraven..."
 until docker compose exec router01 mongosh --host localhost:27017 --eval "db.runCommand({ ping: 1 })" > /dev/null 2>&1; do
   echo "→ router01 ještě není připraven, čekám 2s..."
   sleep 2
 done
-
 
 # === ROUTER ===
 echo "Inicializace routeru (přidání shardů)..."
@@ -52,3 +50,11 @@ echo "Zapnutí shardingu pro RampaBase..."
 docker compose exec router01 mongosh -u "martin" -p "rampouch" --authenticationDatabase admin /scripts/enable-sharding.js
 
 echo "Inicializace MongoDB clusteru hotová!"
+
+
+# === Import data ===
+echo "Importuji data do databáze RampaBase..."
+python3  import_all.py
+
+echo "Import dat do databáze RampaBase hotov!"
+echo "Script manual-cluster-init.sh hotov!"
